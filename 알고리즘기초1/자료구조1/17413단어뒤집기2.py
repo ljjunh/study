08db5aa13a9c94e160a9be67a19d030946836083
ljjunh <limjunhee4575@gmail.com>
2024-02-06 23:21:26 +0900

    2024/02/06

--- 알고리즘기초1/자료구조1/17413단어뒤집기2.py
+++ 알고리즘기초1/자료구조1/17413단어뒤집기2.py
@@ -1,8 +1,18 @@
+from collections import deque
 string = input() + " "
-stack = []
+stack = deque([])
 for i in string:
-    stack.append(i)
-    if i == " ":
+    if i == " " and ">" not in stack and "<" not in stack:
         while stack:
             print(stack.pop(), end = "")
-    
+        print(" ", end = "")
+    elif i == ">":
+        stack.append(i)
+        while stack:
+            print(stack.popleft(), end = "")
+    elif i == "<":
+        while stack:
+            print(stack.pop(), end = "")
+        stack.append(i)
+    else:
+        stack.append(i)
