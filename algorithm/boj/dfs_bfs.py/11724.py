1fc9f4d1e13b1e0bda65c4adbe0a0e7920c86177
임준희 <limjunhee4575@gmail.com>
2024-02-14 10:03:36 +0900

    2024/02/14

--- algorithm/boj/dfs_bfs.py/11724.py
+++ algorithm/boj/dfs_bfs.py/11724.py
@@ -1,25 +1,28 @@
 import sys
-sys.setrecursionlimit(100000) # 안되면 0 하나씩 더 붙여보자
 input = sys.stdin.readline
 
-def dfs(v):
-    visited[v] = True
-    for i in arr[v]:
-        if not visited[i]:
+def dfs(idx):
+    visited[idx] = True
+    for i in range(1, n + 1):
+        if not visited[i] and graph[idx][i]:
             dfs(i)
-    
-n, m = map(int, input().split()) # n = 정점, m = 간선
-arr = [[] for _ in range(n+1)]
-visited = [False] * (n + 1) # 정점의 개수보다 1개 더 (인덱스별로 쓰려고 일부러 인덱스0은 비워둠)
-for _ in range(m): # 간선의 개수만큼 반복
-    s, e = map(int, input().split())
-    arr[s].append(e) # 양방향 그래프여서 양쪽에 다 추가
-    arr[e].append(s)
 
+
+# 0. 입력받기, visited, graph 초기화
+n, m = map(int, input().split()) # n : 노드   m : 엣지
+graph = [[False] * (n + 1) for _ in range(n+1)]
+visited = [False] * (n + 1)
 cnt = 0
-
+# 1. 그래프 입력
+for i in range(m):
+    x, y = map(int, input().split())
+    graph[x][y] = True
+    graph[y][x] = True
+    
+# 2. dfs호출
 for i in range(1, n + 1):
-    if not visited[i]:  # 연결 노드 중 방문하지 않았던 정점만 탐색
+    if not visited[i]:
         cnt += 1
         dfs(i)
-print(cnt)
+# 3. 출력
+print(cnt)        
--- algorithm/boj/dfs_bfs.py/2606.py
+++ algorithm/boj/dfs_bfs.py/2606.py
@@ -1,17 +1,27 @@
-def dfs(v):
-    visited[v] == 1
-    for i in graph[v]:
-        if not visited[i]:
+import sys
+input = sys.stdin.readline
+def dfs(idx):
+    global answer
+    visited[idx] = True
+    answer += 1
+
+    for i in range(1, n + 1):
+        if not visited[i] and graph[idx][i]:
             dfs(i)
 
+# 0. 입력받기
 n = int(input()) # 노드
-m = int(input()) # 간선
-graph = [[] for _ in range(n+1)]
-visited = [0] * (n+1)
+m = int(input()) # 엣지
+graph = [[False] * (n + 1) for _ in range(n + 1)]
+visited = [False] * (n + 1)
+answer = 0
+# 1. 그래프 정보 입력
 for i in range(m):
-    s, e = map(int, input().split())
-    graph[s].append(e)
-    graph[e].append(s)
+    x, y = map(int, input().split())
+    graph[x][y] = True
+    graph[y][x] = True
 
+# 2. dfs 호출
 dfs(1)
-print(visited)    
+# 3. 출력
+print(answer - 1)
