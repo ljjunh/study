473447e9425369a685fd184bd14a3b1e7898c764
임준희 <limjunhee4575@gmail.com>
2024-02-28 17:59:33 +0900

    24/02/28

--- algorithm/swea/0228/베이비진게임.py
+++ algorithm/swea/0228/베이비진게임.py
@@ -12,30 +12,33 @@ for tc in range(1, T + 1):
     arr1 = player_1[:2]
     arr2 = player_2[:2]
     cnt = 0
-    visited1 = [False] * 10
-    visited1[player_1[0]] = True
-    visited1[player_1[1]] = True
+    visited1 = [0] * 10
+    visited1[player_1[0]] += 1
+    visited1[player_1[1]] += 1
     visited2 = [False] * 10
-    visited2[player_2[0]] = True
-    visited2[player_2[1]] = True
+    visited2[player_2[0]] += 1
+    visited2[player_2[1]] += 1
+    
     for i in range(2, len(player_1)):
         arr1.append(player_1[i])
         arr2.append(player_2[i])
-        visited1[player_1[i]] = True
-        visited2[player_2[i]] = True
+        visited1[player_1[i]] += 1
+        visited2[player_2[i]] += 1
+        if 3 in visited1:
+            print(f"#{tc} 1")
+            break
+        if 3 in visited2:
+            print(f"#{tc} 2")
+            break
+        
         for j in range(8):
             if visited1[j] and visited1[j+1] and visited1[j+2]:
-                cnt += 1
+                print(f"#{tc} 1")
+                exit()
+             
             if visited2[j] and visited2[j+1] and visited2[j+2]:
-                cnt += 2
-
-        if arr1.count(player_1[i]) == 3:
-            cnt += 1
-        if arr2.count(player_2[i]) == 3:
-            cnt += 2
-        
-        if cnt >= 1:
-            print(f"#{tc} {cnt}")
-            break
+                print(f"#{tc} 2")
+                exit()
     else:
         print(f"#{tc} 0")
+        
