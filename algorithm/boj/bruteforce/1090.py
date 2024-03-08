fd785aad0e03bc00e09a9de7f105a9af00d9d37e
임준희 <limjunhee4575@gmail.com>
2024-03-08 12:01:36 +0900

    24/03/08

--- algorithm/boj/bruteforce/1090.py
+++ algorithm/boj/bruteforce/1090.py
@@ -1,25 +1,29 @@
+# 어차피 모일때 최단거리는 x,y조합중에 하나임
 N = int(input())
 arr = []
 arr_x = []
 arr_y = []
 ans = [-1] * N
+# 입력받기
 for _ in range(N):
-    x, y = map(int, input().split())
+    x, y = map(int ,input().split())
     arr_x.append(x)
     arr_y.append(y)
-    arr.append([x, y])
-
-for i in arr_x:
-    for j in arr_y:
+    arr.append((x,y))
+    
+for x in arr_x:
+    for y in arr_y:
         temp = []
         for dx, dy in arr:
-            temp.append(abs(dx-i) + abs(dy-j))
+            temp.append(abs(dx-x) + abs(dy-y))
         temp.sort()
+
         cnt = 0
-        for k in range(len(temp)):
-            cnt += temp[k]
-            if ans[k] == -1:
-                ans[k] = cnt
+        for i in range(len(temp)):
+            cnt += temp[i]
+            if ans[i] == -1:
+                ans[i] = cnt
             else:
-                ans[k] = min(ans[k], cnt)
+                ans[i] = min(ans[i], cnt)
 print(*ans)
+
