070b7d607f336e51765e6c8d07944cf840edf29f
임준희 <limjunhee4575@gmail.com>
2024-03-13 17:48:24 +0900

    24/03/13

--- algorithm/boj/bruteforce/2503recur.py
+++ algorithm/boj/bruteforce/2503recur.py
@@ -1,43 +1,71 @@
 import sys
-def recur(cnt, num):
-    if cnt == N:
-        recur(0, num+1)
+
+input = sys.stdin.readline
+sys.setrecursionlimit(1000000)
+
+
+def checker(idx, num):
+    _num = arr[idx][0]
+    _strike = arr[idx][1]
+    _ball = arr[idx][2]
+
+    strike = 0
+    ball = 0
+
+    _A = _num // 100
+    _B = (_num - (_A * 100)) // 10
+    _C = _num % 10
+
+    A = num // 100
+    B = (num - (A * 100)) // 10
+    C = num % 10
+
+    if A == 0 or B == 0 or C == 0:
+        return False
+
+    if A == B or A == C or B == C:
+        return False
+
+    if A == _A:
+        strike += 1
+    if B == _B:
+        strike += 1
+    if C == _C:
+        strike += 1
+
+    if A == _B or A == _C:
+        ball += 1
+    if B == _A or B == _C:
+        ball += 1
+    if C == _A or C == _B:
+        ball += 1
+
+    if strike == _strike and ball == _ball:
+        return True
+    return False
+
+
+def recur(idx, num):
+    global ans
+
+    if idx == n:
         ans += 1
+
+        recur(0, num + 1)
         return
 
     if num == 1000:
         return
-    
-    strike_count = 0
-    ball_count = 0
-    a = num // 100
-    b = (num // 10) // 10
-    c = num % 10
-    if a == arr[cnt][0]//100 :
-        strike_count += 1
-    if a == (arr[cnt][0]//10)//10 or a == arr[cnt][0] % 10:
-        ball_count += 1
-    if b == arr[cnt][0] // 10:
-        strike_count += 1
-    if b == arr[cnt][0]//100 or b == arr[cnt][0] % 10:
-        ball_count += 1
-    if c == arr[cnt][0] % 10:
-        strike_count += 1
-    if c == arr[cnt][0] // 100 or (arr[cnt][0]//10) // 10:
-        ball_count += 1
-    
 
-    # 만약 힌트를 통과했다면
-    # if 스트라이크 카운트랑 볼 카운트가 입력값이랑 동일하다면 
-    if strike_count == arr[cnt][1] and ball_count == arr[cnt][2]:
-        recur(cnt+1, num)
-    # 만약 힌트에 통과하지 못한다면
+    if checker(idx, num):
+        recur(idx + 1, num)
     else:
-        recur(0, num+1)
+        recur(0, num + 1)
+
 
 input = sys.stdin.readline
-N = int(input())
-arr = [list(map(int, input().split())) for _ in range(N)]
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
 ans = 0
 recur(0, 100)
 print(ans)
