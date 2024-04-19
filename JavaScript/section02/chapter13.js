80458e43c946e47465eb9a34a1470f6d3b334638
임준희 <dlawnsdlekd@naver.com>
2024-04-19 17:44:13 +0900

    24/04/19

--- JavaScript/section02/chapter13.js
+++ JavaScript/section02/chapter13.js
@@ -1,8 +1,32 @@
-const promise = new Promise((resolve, reject) => {
-  // 비동기 작업을 실행하는 함수(executor)
-  setTimeout(() => {
-    console.log("안녕");
-  }, 2000);
-});
+function add10(num) {
+  const promise = new Promise((resolve, reject) => {
+    // 비동기 작업 실행하는 함수 executor
+    setTimeout(() => {
+      if (typeof num === "number") {
+        resolve(num + 10);
+      } else {
+        reject("num이 숫자가 아닙니다.");
+      }
+    }, 2000);
+  });
+  return promise;
+}
 
-console.log(promise);
+add10(0)
+  .then((result) => {
+    console.log(result);
+
+    return add10(result);
+  })
+  .then((result) => {
+    console.log(result);
+    return add10(result);
+  })
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+// promise가 성공하면 then에 있는 콜백함수 실행
+// 실패하면 then은 실행x, catch 실행
