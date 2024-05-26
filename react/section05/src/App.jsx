49b44f7c082b9330dc1bed2d9dcd7773fcef5d88
junhee <limjunhee4575@gmail.com>
2024-05-26 23:36:14 +0900

    24/05/26

--- react/section05/src/App.jsx
+++ react/section05/src/App.jsx
@@ -2,13 +2,22 @@ import "./App.css";
 import Header from "./components/Header"; // Header.jsx를 가져오기 vite에서는 확장자 생략 가능
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import Button from "./components/Button";
 function App() {
+  const buttonProps = {
+    text: "메일",
+    color: "red",
+    a: 1,
+    b: 2,
+    c: 3,
+  };
   return (
     <>
-      <Header />
-      <Main />
-      <h1>안녕 리액트!</h1>
-      <Footer />
+      <Button {...buttonProps} />
+      <Button text={"카페"} />
+      <Button text={"블로그"}>
+        <Header />
+      </Button>
     </>
   );
 }
