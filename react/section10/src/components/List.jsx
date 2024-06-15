36cfa16c6dbd5c1d86be846ca006f6dcede14c72
junhee <limjunhee4575@gmail.com>
2024-06-15 19:32:06 +0900

    24/06/15

---
+++ react/section10/src/components/List.jsx
@@ -0,0 +1,59 @@
+import "./List.css";
+import TodoItem from "./TodoItem";
+import { useState, useMemo } from "react";
+const List = ({ todos, onUpdate, onDelete }) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter((todo) => {
+      return todo.content.toLowerCase().includes(search.toLowerCase());
+    });
+  };
+
+  const filteredTodos = getFilteredData();
+
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    console.log("getAnalyzedData 호출");
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+    return { totalCount, doneCount, notDoneCount };
+  }, [todos]);
+  // 콜백함수, deps
+  // deps의 값이 변경될때마다 콜백함수 실행
+
+  return (
+    <div className="List">
+      <h4>Todo List🌱</h4>
+      <div>total:{totalCount}</div>
+      <div>done: {doneCount}</div>
+      <div>notDone: {notDoneCount}</div>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
+
+      <div className="todos_wrapper">
+        {filteredTodos.map((todo) => {
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+export default List;
---
+++ react/section10/src/components/TodoItem.jsx
@@ -0,0 +1,38 @@
+import "./TodoItem.css";
+import { memo } from "react";
+
+const TodoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
+  const onChangeCheckbox = () => {
+    onUpdate(id);
+  };
+  const onClickDeleteButton = () => {
+    onDelete(id);
+  };
+  return (
+    <div className="TodoItem">
+      <input
+        onChange={onChangeCheckbox}
+        type="checkbox"
+        checked={isDone}
+        readOnly
+      />
+      <div className="content">{content}</div>
+      <div className="date">{new Date(date).toLocaleDateString()}</div>
+      <button onClick={onClickDeleteButton}>삭제</button>
+    </div>
+  );
+};
+
+// 고차 컴포넌트 (HOC)
+// export default memo(TodoItem, (prevProps, nextProps) => {
+//   // 반환값에 따라 , props가 바뀌었는지 판단
+//   // T => Props 바뀌지 않음 -> 리렌더링x
+//   // F => Props 바뀜 -> 리렌더링o
+//   if (prevProps.id !== nextProps.id) return false;
+//   if (prevProps.isDone !== nextProps.isDone) return false;
+//   if (prevProps.content !== nextProps.content) return false;
+//   if (prevProps.date !== nextProps.date) return false;
+//   // 만약 아무것도 안바뀌면 true반환해서 리렌더링 되지 않도록
+//   return true;
+// });
+export default memo(TodoItem);
