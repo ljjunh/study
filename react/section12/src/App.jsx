1ada38063770dfce6549830b3cd4fc5af7a3d404
junhee <limjunhee4575@gmail.com>
2024-06-17 19:18:16 +0900

    24/06/17

---
+++ react/section12/src/App.jsx
@@ -0,0 +1,101 @@
+import "./App.css";
+import { useReducer, useRef, createContext } from "react";
+import { Route, Routes } from "react-router-dom";
+import Diary from "./pages/Diary";
+import Edit from "./pages/Edit";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Notfound from "./pages/Notfount";
+
+const mokData = [
+  {
+    id: 1,
+    createdDate: new Date("2024-06-16").getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date("2024-06-15").getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+  {
+    id: 3,
+    createdDate: new Date("2024-05-08").getTime(),
+    emotionId: 3,
+    content: "3번 일기 내용",
+  },
+];
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) =>
+        String(item.id) === String(action.data.id) ? action.data : item
+      );
+    case "DELETE":
+      return state.filter((item) => String(item.id) !== String(action.id));
+    default:
+      return state;
+  }
+}
+
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
+
+function App() {
+  const [data, dispatch] = useReducer(reducer, mokData);
+  const idRef = useRef(3);
+  const onCreate = (createdDate, emotionId, content) => {
+    // 새로운 일기 추가
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  // 기존 일기 수정
+  const onUpdate = (id, createdDate, emotionId, content) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  // 기존 일기 삭제
+  const onDelete = (id) => {
+    dispatch({
+      type: "DELETE",
+      id,
+    });
+  };
+  return (
+    <>
+      <DiaryStateContext.Provider value={data}>
+        <DiaryDispatchContext.Provider value={{ onCreate, onUpdate, onDelete }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/new" element={<New />} />
+            <Route path="/diary/:id" element={<Diary />} />
+            <Route path="/edit/:id" element={<Edit />} />
+            <Route path="*" element={<Notfound />} />
+          </Routes>
+        </DiaryDispatchContext.Provider>
+      </DiaryStateContext.Provider>
+    </>
+  );
+}
+export default App;
---
+++ react/section12/src/components/DiaryItem.jsx
@@ -0,0 +1,28 @@
+import "./DiaryItem.css";
+import { getEmotionImage } from "../util/get-emotion-image";
+import Button from "./Button";
+import { useNavigate } from "react-router-dom";
+
+const DiaryItem = ({ id, emotionId, createDate, content }) => {
+  const nav = useNavigate();
+  return (
+    <div className="DiaryItem">
+      <div
+        onClick={() => nav(`/diary/${id}`)}
+        className={`img_section img_section_${emotionId}`}
+      >
+        <img src={getEmotionImage(emotionId)} />
+      </div>
+      <div className="info_section" onClick={() => nav(`/diary/${id}`)}>
+        <div className="created_date">
+          {new Date(createDate).toLocaleDateString()}
+        </div>
+        <div className="content">{content}</div>
+      </div>
+      <div className="button_section">
+        <Button onClick={() => nav(`/edit/${id}`)} text={"수정하기"} />
+      </div>
+    </div>
+  );
+};
+export default DiaryItem;
