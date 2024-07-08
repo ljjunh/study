6241402640f5aac100f05130b7258aaeeee2a853
junhee <limjunhee4575@gmail.com>
2024-07-08 22:33:19 +0900

    24/07/08

---
+++ TypeScript/section10/src/chapter2.ts
@@ -0,0 +1,73 @@
+/**
+ * Pick<T, K>
+ * -> 뽑다, 고르다
+ * -> 객체 타입으로부터 특정 프로퍼티만 딱 골라내는 타입
+ */
+
+interface Post {
+  title: string;
+  tags: string[];
+  content: string;
+  thumbnailURL?: string;
+}
+
+type Pick<T, K extends keyof T> = {
+  // K extends 'title' | 'tags' | 'content' | 'thumbnailURL'
+  //'title' | 'content' extends 'title' | 'tags' | 'content' | 'thumbnailURL'
+  [key in K]: T[key];
+};
+
+const legacyPost: Pick<Post, "title" | "content"> = {
+  title: "옛날 글",
+  content: "옛날 컨텐츠",
+};
+
+/**
+ * Omit<T, K>
+ * -> 생략하다, 빼다
+ * -> 객체 타입으로부터 특정 프로퍼티를 제거하는 타입
+ */
+
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+// T = Post, K= "title"
+// Pick<Post, Exclude<keyof Post, 'title>>
+// Pick<Post, Exclude<'title' | 'content' | 'tags' | 'thumbnailURL', 'title'>>
+// Pick<Post, 'content' | 'tags' | 'thumbnailURL'>
+
+const noTitlePost: Omit<Post, "title"> = {
+  content: "",
+  tags: [],
+  thumbnailURL: "",
+};
+
+/**
+ * Record<K, V>
+ *
+ */
+
+type ThumbnailLegacy = {
+  large: {
+    url: string;
+  };
+  medium: {
+    url: string;
+  };
+  small: {
+    url: string;
+  };
+  watch: {
+    url: string;
+  };
+};
+
+type Record<K extends keyof any, V> = {
+  [key in K]: V;
+};
+
+// 위에꺼랑 같음
+// 첫번째 타입에는 객체의 프로퍼티 key를 유니온타입으로 받고
+// 두번째 타입에는 키들의 value 타입을 받음
+type Thumbnail = Record<
+  "large" | "medium" | "small" | "watch",
+  { url: string; size: number }
+>;
