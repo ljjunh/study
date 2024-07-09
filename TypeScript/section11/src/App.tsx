befc3a52ea4c8f74a4e4b3a3aab790a3ef465458
limjunhee <limjunhee4575@gmail.com>
2024-07-09 17:43:20 +0900

    24/07/09

--- TypeScript/section11/src/App.tsx
+++ TypeScript/section11/src/App.tsx
@@ -1,7 +1,39 @@
+import { useState, useRef, useEffect } from "react";
 import "./App.css";
+import Editor from "./components/Editor";
+import { Todo } from "./types";
+import TodoItem from "./components/TodoItem";
 
 function App() {
-  return <div className="App"></div>;
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const idRef = useRef(0);
+  const onClickAdd = (text: string) => {
+    setTodos([
+      ...todos,
+      {
+        id: idRef.current++,
+        content: text,
+      },
+    ]);
+  };
+
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
+
+  return (
+    <div className="App">
+      <h1>Todo</h1>
+      <Editor onClickAdd={onClickAdd}>
+        <div>child</div>
+      </Editor>
+      <div>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} {...todo}></TodoItem>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
